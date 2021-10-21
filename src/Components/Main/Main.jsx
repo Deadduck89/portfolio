ac0f4e6994da543362f5cc0ee4312fabee40e4d1
Deadduck89 <deadduck89@yandex.ru>
2021-10-21 16:11:40 +0300

    App grid, background colors, starter components

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Main.module.css'
+
+const Main = () => {
+    return (
+        <div className={classes.main}>
+            Main
+        </div>
+    )
+}
+
+export default Main;
