013d96d6bc727551f3a456d7b0e156bb5c0b0632
Deadduck89 <deadduck89@yandex.ru>
2021-10-27 16:53:19 +0300

    Headers in main page and avatar

--- src/Components/Content/MainPage/MainPage.jsx
+++ src/Components/Content/MainPage/MainPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import classes from './MainPage.module.css'
+import Avatar from "@mui/material/Avatar";
+import avatar from "../../../Assets/Images/3vjrzd2.png";
 
 const MainPage = () => {
     return (
         <div className={classes.main}>
-            Main
+            <div className={classes.hello}>
+                <h1>Привет, меня зовут Михаил Дорогин</h1>
+                <h2> Я фронт энд разработчик</h2>
+            </div>
+            <div className={classes.avatar}>
+                <Avatar
+                    alt='my avatar'
+                    src={avatar}
+                    sx={{width: 200, height: 200}}
+                />
+            </div>
         </div>
     )
 }
