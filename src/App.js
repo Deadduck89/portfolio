2bd653c020e18af94c6e91277a61c29586f6530f
Deadduck89 <deadduck89@yandex.ru>
2021-10-27 16:07:08 +0300

    Roboto font import in App.js
    Damn font is now working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import "@fontsource/open-sans";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import classes from './App.module.css';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
