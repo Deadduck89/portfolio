2de89a75d4bf4caeb1cf3e7795bcb44919291125
Deadduck89 <deadduck89@yandex.ru>
2021-10-28 16:49:58 +0300

    Light side of main page
    Minor changes in header styles, Manrope font

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import "@fontsource/aclonica";
+import "@fontsource/manrope";
 import classes from './App.module.css';
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
