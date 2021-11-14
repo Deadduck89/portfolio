c45ebf98dfb7edf312240a0a1f1dbe70b7b3252f
Deadduck89 <deadduck89@yandex.ru>
2021-11-14 19:54:12 +0300

    Fleacker to h1,h2 main content dark

--- src/Components/Content/MainPage/MainContentDark/MainContentDark.jsx
+++ src/Components/Content/MainPage/MainContentDark/MainContentDark.jsx
@@ -1,11 +1,12 @@
 import classes from "../MainPage.module.css";
 import React from "react";
+import styles from './MainContentDark.module.css';
 
 const MainContentDark = () => {
     return (
             <div className={classes.hello}>
-                <h1>Меня всё ещё зовут Михаил Дорогин.</h1>
-                <h2> И это моя темная сторона разработчика</h2>
+                <h1 className={styles.h1}>Меня всё ещё зовут Михаил Дорогин.</h1>
+                <h2 className={styles.h2}> И это моя темная сторона разработчика</h2>
                 <div className={classes.helloTextBlock}>
                     <p>Мне нравится создавать интерактивные и приковывающие внимание интерфейсы, используя
                         Реакт</p>
