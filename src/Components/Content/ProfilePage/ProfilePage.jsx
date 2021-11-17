37adda17ed7f0e9f3a58dda332456f270c5ac02d
Deadduck89 <deadduck89@yandex.ru>
2021-11-17 16:52:47 +0300

    Start of profile page

--- src/Components/Content/ProfilePage/ProfilePage.jsx
+++ src/Components/Content/ProfilePage/ProfilePage.jsx
@@ -5,6 +5,35 @@ const ProfilePage = () => {
     return (
         <div className={classes.main}>
             Profile
+            <div className={classes.photo}>
+                <img src='' alt=''/>
+            </div>
+            <div className={classes.info}>
+                <div className={classes.infoPersonal}>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className={classes.infoProgramming}>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className={classes.infoProjects}>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
