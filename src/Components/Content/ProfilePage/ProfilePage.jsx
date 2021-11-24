08286e0e04a4c2d58925078293fb686088680bbb
Deadduck89 <deadduck89@yandex.ru>
2021-11-24 15:05:34 +0300

    Dark theme in profile

--- src/Components/Content/ProfilePage/ProfilePage.jsx
+++ src/Components/Content/ProfilePage/ProfilePage.jsx
@@ -1,34 +1,38 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import classes from './ProfilePage.module.css'
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import ProfileInfoContainer from "./ProfileInfoContainer/ProfileInfoContainer";
 import photo from '../../../Assets/Images/3vjrzd2.png';
+import MyThemeContext from "../../Common/ThemeContext/ThemeContext";
 
 const ProfilePage = () => {
+
+    const value = useContext( MyThemeContext );
+    const theme = value.theme;
+
+    let mainStyle = classes.main;
+
+    const isLight = (theme === 'light') ?  (mainStyle = classes.main) : (mainStyle = classes.mainDark);
 
     const [page,setPage] = useState('personalInfo');
 
     const changePagePersonal = () => {
         setPage('personalInfo')
     };
-    const changePageProgramming = () => {
-        setPage('programmingInfo')
-    };
     const changePageProjects = () => {
         setPage('projectsInfo')
     };
 
     return (
-        <div className={classes.main}>
+        <div className={mainStyle}>
             <div className={classes.photo}>
                 <img src={photo} alt='me'/>
             </div>
             <div className={classes.info}>
                 <div className={classes.infoButtons}>
                     <ButtonGroup variant="outlined" aria-label="outlined button group">
-                        <Button onClick={changePagePersonal}>Личное</Button>
-                        <Button onClick={changePageProgramming}>Программирование</Button>
+                        <Button onClick={changePagePersonal}>О себе</Button>
                         <Button onClick={changePageProjects}>Проекты</Button>
                     </ButtonGroup>
                 </div>
