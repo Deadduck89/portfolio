d5356ed9d5cbe0f9efec1199ed7c98215c6cdc8e
Deadduck89 <deadduck89@yandex.ru>
2021-11-25 15:12:53 +0300

    Added my projects in component

--- src/Components/Content/ProfilePage/ProfileInfoContainer/ProjectsInfo/ProjectsInfo.jsx
+++ src/Components/Content/ProfilePage/ProfileInfoContainer/ProjectsInfo/ProjectsInfo.jsx
@@ -3,13 +3,22 @@ import classes from "../../ProfilePage.module.css";
 
 const ProjectsInfo = () => {
     return (
-        <div className={classes.infoPersonal}>
+        <div className={classes.infoProjects}>
             <h2>Проекты</h2>
             <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>
+                    <a href='https://deadduck89.github.io/react-social-network-demo/'>Проект социальной сети на React и
+                        Redux</a>
+                    <p>Для логина используйте тестовый аккаунт: Email:free@samuraijs.com Password: free</p>
+                </li>
+                <li>
+                    <a href='https://deadduck89.github.io/adaptive-landing-demo/'>Проект с применением адаптивной
+                        верстки, SASS и Bootstrap</a>
+                </li>
+                <li>
+                    <a href='https://pulse.mdorogin.ru/'>Одностраничник с применением БЭМ, SCSS, форм, google maps API,
+                        модальных окон с возможностью отправки заявки на e-mail</a>
+                </li>
             </ul>
         </div>
     )
