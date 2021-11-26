dd590cdc1b8fd2011909aba96f980151f87c48fe
Deadduck89 <deadduck89@yandex.ru>
2021-11-26 13:22:39 +0300

    Links to projects open in new tabs

--- src/Components/Content/ProfilePage/ProfileInfoContainer/ProjectsInfo/ProjectsInfo.jsx
+++ src/Components/Content/ProfilePage/ProfileInfoContainer/ProjectsInfo/ProjectsInfo.jsx
@@ -7,16 +7,16 @@ const ProjectsInfo = () => {
             <h2>Проекты</h2>
             <ul>
                 <li>
-                    <a href='https://deadduck89.github.io/react-social-network-demo/'>Проект социальной сети на React и
+                    <a href='https://deadduck89.github.io/react-social-network-demo/' target="_blank">Проект социальной сети на React и
                         Redux</a>
                     <p>Для логина используйте тестовый аккаунт: Email:free@samuraijs.com Password: free</p>
                 </li>
                 <li>
-                    <a href='https://deadduck89.github.io/adaptive-landing-demo/'>Проект с применением адаптивной
+                    <a href='https://deadduck89.github.io/adaptive-landing-demo/' target="_blank">Проект с применением адаптивной
                         верстки, SASS и Bootstrap</a>
                 </li>
                 <li>
-                    <a href='https://pulse.mdorogin.ru/'>Одностраничник с применением БЭМ, SCSS, форм, google maps API,
+                    <a href='https://pulse.mdorogin.ru/' target="_blank">Одностраничник с применением БЭМ, SCSS, форм, google maps API,
                         модальных окон с возможностью отправки заявки на e-mail</a>
                 </li>
             </ul>
