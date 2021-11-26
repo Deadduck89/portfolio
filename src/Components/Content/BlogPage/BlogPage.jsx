8146071f0dba651603b8a397f0dae1fb47121909
Deadduck89 <deadduck89@yandex.ru>
2021-11-26 13:54:20 +0300

    Blog page template

--- src/Components/Content/BlogPage/BlogPage.jsx
+++ src/Components/Content/BlogPage/BlogPage.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import classes from './BlogPage.module.css'
+import MyThemeContext from "../../Common/ThemeContext/ThemeContext";
 
 const BlogPage = () => {
+
+    const value = useContext( MyThemeContext );
+    const theme = value.theme;
+
+    let blogStyle = (theme === 'light') ?  (classes.blog) : (classes.blogDark);
+
+
     return (
-        <div className={classes.main}>
-            Blog
+        <div className={blogStyle}>
+            <div className={classes.blogList}>Список статей</div>
+            <div className={classes.blogText}>Статья</div>
         </div>
     )
 }
